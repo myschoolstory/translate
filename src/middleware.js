2d2a07ee499ace4238bb0626a021879d54163929
myschoolstory <168579347+myschoolstory@users.noreply.github.com>
2024-12-15 19:46:35 -0500

    Create middleware.js

---
+++ src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "8d36c79d-c3b5-4a2f-8e79-ccbd14799777");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
